2d3e41d822ff05a20de0794a7ed720ac0e569e42
Carter Rose <CarterRose@Carters-MacBook-Pro.local>
2019-03-21 15:17:30 -0400

    testing again

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Carter Rose`s Heroku Test App', message: "Welcome to your first Express app", productDesc: "SHINE BRIGHTLY, BREATHE DEEPLY.Infuse your air with the vitality of nature and create a soothing atmosphere by diffusing 100% natural essential oils. Add a splash of chic to any space with this decorative gem." });
+  res.render('index', { title: 'Carter Rose`s Heroku Test App', message: "AROMA GEM ULTRASONIC DIFFUSER BY SAJE" });
 });
+
+var context = {
+  productDesc: "SHINE BRIGHTLY, BREATHE DEEPLY. <br><br> Infuse your air with the vitality of nature and create a soothing atmosphere by diffusing 100% natural essential oils. Add a splash of chic to any space with this decorative gem.",
+  howToUse: "1. Fill water tank to maximum water level line. <br><br> 2. Add 10-15 drops of your favourite Saje diffuser blend. <br><br> 3. Press diffuser’s on button and breathe in the benefits of nature.",
+};
 
 module.exports = router;
