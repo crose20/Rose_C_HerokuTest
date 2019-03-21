67bd4e7bed971dbde5f6ebd769c8528f83ef2094
Carter Rose <CarterRose@Carters-MacBook-Pro.local>
2019-03-21 15:31:05 -0400

    starting to style

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,9 @@ router.get('/', function(req, res, next) {
 });
 
 var context = {
-  productDesc: "SHINE BRIGHTLY, BREATHE DEEPLY. <br><br> Infuse your air with the vitality of nature and create a soothing atmosphere by diffusing 100% natural essential oils. Add a splash of chic to any space with this decorative gem.",
-  howToUse: "1. Fill water tank to maximum water level line. <br><br> 2. Add 10-15 drops of your favourite Saje diffuser blend. <br><br> 3. Press diffuser’s on button and breathe in the benefits of nature.",
-  benefits: "Purifies and humidifies while infusing your air with the healing power of plants. Releases negative ions to reduce dust, pet dander and other airborne allergens (like dust mite matter). Refreshes the air in your space using 100% natural essential oil blends. Preserves the full integrity and properties of your essential oil blends using a heat-free system. Gives your atmosphere the same refreshing feeling as standing near a waterfall."
-
+  productDesc: "SHINE BRIGHTLY, BREATHE DEEPLY. Infuse your air with the vitality of nature and create a soothing atmosphere by diffusing 100% natural essential oils. Add a splash of chic to any space with this decorative gem.",
+  howToUse: "1. Fill water tank to maximum water level line. 2. Add 10-15 drops of your favourite Saje diffuser blend. 3. Press diffuser’s on button and breathe in the benefits of nature.",
+  benefits: "Purifies and humidifies while infusing your air with the healing power of plants. Releases negative ions to reduce dust, pet dander and other airborne allergens (like dust mite matter). Refreshes the air in your space using 100% natural essential oil blends. Preserves the full integrity and properties of your essential oil blends using a heat-free system. Gives your atmosphere the same refreshing feeling as standing near a waterfall.",
 };
 
 module.exports = router;
