3d0f15f12b6ee3874cbd5c00b6f07b0a8264c38f
Carter Rose <CarterRose@Carters-MacBook-Pro.local>
2019-03-21 15:10:27 -0400

    testing

--- testBuild/routes/index.js
+++ testBuild/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Carter Rose`s Heroku Test App', message: "Welcome to your first Express app", productDesc: "SHINE BRIGHTLY, BREATHE DEEPLY.Infuse your air with the vitality of nature and create a soothing atmosphere by diffusing 100% natural essential oils. Add a splash of chic to any space with this decorative gem." },);
+  res.render('index', { title: 'Carter Rose`s Heroku Test App', message: "Welcome to your first Express app", productDesc: "SHINE BRIGHTLY, BREATHE DEEPLY. Infuse your air with the vitality of nature and create a soothing atmosphere by diffusing 100% natural essential oils. Add a splash of chic to any space with this decorative gem." },);
 });
 
 module.exports = router;
